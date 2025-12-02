b325809116b7157cfda3d6b0db80de92b1c4d177
Tobert03 <tobias.72518@protonmail.com>
2025-12-02 21:49:52 +0100

    added insert method for new items from my api to the table inventory

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Union
+import sqlite3
 
 
 #"uvicorn fastapi_app:app --reload --port 8000" in cmd, api kann dann unter http://127.0.0.1:8000 erreicht werden
@@ -9,3 +10,18 @@ app = FastAPI()
 @app.get("/text")
 def root():
   return "das ist der text"
+
+
+
+@app.put("/new_item/{product_name}")
+def new_item(product_name: str):
+  conn = sqlite3.connect("database.db")
+  cursor = conn.cursor()
+  try:
+    cursor.execute( "INSERT INTO inventory (product_name) VALUES (?)", (product_name,) )
+
+    conn.commit()
+
+    return f"Neues Item '{product_name}' wurde eingefügt"
+  except Exception as e:
+    return f"Item '{product_name}' konnte nicht hinzugefügt werden: {e}"
